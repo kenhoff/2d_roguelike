c35eb58a74c21fa444aeb48ad95570dd8c3920d0
Ken Hoff <thekenhoff@gmail.com>
2015-08-13 22:46:28 -0700

    end of lesson 8

---
+++ Assets/Scripts/Wall.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+public var dmgSprite : Sprite;
+public var hp : int = 4;
+
+private var spriteRenderer : SpriteRenderer;
+
+function Awake () {
+    spriteRenderer = GetComponent.<SpriteRenderer>();
+}
+
+function DamageWall(loss : int) {
+    spriteRenderer.sprite = dmgSprite;
+    hp -= loss;
+    if (hp <= 0) {
+        gameObject.SetActive(false);
+    }
+}
